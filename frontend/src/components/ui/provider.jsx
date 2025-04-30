e615b1edf88f9abdb1d674f48278c4bdecf5300d
Jas2365 <jastha6wdc@gamil.com>
2025-04-30 15:40:27 +0530

    finishing changes

--- frontend/src/components/ui/provider.jsx
+++ frontend/src/components/ui/provider.jsx
@@ -1,15 +1,3 @@
-// 'use client'
-
-// import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-// import { ColorModeProvider } from './color-mode'
-
-// export function Provider(props) {
-//   return (
-//     <ChakraProvider value={defaultSystem}>
-//       <ColorModeProvider {...props} />
-//     </ChakraProvider>
-//   )
-// }
 "use client";
 
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
